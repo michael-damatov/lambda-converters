aff334080a562e5ebc7c2bb07d1f48d3307f5eab
Dima Enns <dieter.enns@gmail.com>
2017-10-03 15:37:12 +0200

    .Net 4.5 compatibility
    Signed-off-by: Dima Enns <dieter.enns@gmail.com>

--- Sources/LambdaConverters.Wpf/Properties/AssemblyInfo.cs
+++ Sources/LambdaConverters.Wpf/Properties/AssemblyInfo.cs
@@ -4,12 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LambdaConverters.Wpf")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LambdaConverters")]
-[assembly: AssemblyCopyright("© 2017 Michael Damatov, Dimitri Enns.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8de93d00-4b56-424b-aa7d-02c4a4a41f0d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
